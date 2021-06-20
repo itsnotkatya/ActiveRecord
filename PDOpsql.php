a458491dda199d66b3ca2ca3389f6d757b6eee98
Катя Заплатина <49903775+itsnotkatya@users.noreply.github.com>
2021-06-20 23:22:39 +1000

    updated

--- PDOpsql.php
+++ PDOpsql.php
@@ -3,12 +3,13 @@
 function connect(): PDO
 {
     $host='localhost';
-    $db = 'sit';
-    $user = 'postgres';
-    $password = 'password';
-    try {
-        $dsn = "pgsql:host=$host;port=5432;dbname=$db;";
-
+    $db = 'labs';
+    $user = 'admin';
+    $password = 'admin';
+   try { $dsn = "mysql:host=$host;port=3306;dbname=$db;";
+        return new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    }
+    catch (PDOException $e) {die($e->getMessage());}
         return new PDO(
             $dsn,
             $user,
